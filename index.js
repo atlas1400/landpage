e341159c02524ae58f6393a473872623b05291ab
Matheus Bernardes <abneroliveiralima99@gmail.com>
2023-11-07 18:01:51 -0300

    nova versao

---
+++ index.js
@@ -0,0 +1,22 @@
+var setadireita = window.document.getElementById("setadireita")
+var leonardo = window.document.getElementById("leonardo")
+var samanta = window.document.getElementById("samanta")
+var bruna = window.document.getElementById("bruna")
+var setaesquerda = window.document.getElementById("setaesquerda")
+
+function rolarparadireita(){
+
+    leonardo.style.display = "none";
+    bruna.style.display = "flex";
+    setadireita.style.display = "none";
+    setaesquerda.style.display = "flex";
+    setaesquerda.style.marginTop = "25px";
+}
+
+function rolarparaesquerda(){
+    leonardo.style.display = "flex"
+    bruna.style.display = "none"
+    setadireita.style.display = "flex"
+    setadireita.style.marginTop = "25px"
+    setaesquerda.style = "none"
+}
